eba854388426f5de57f9fe4afe25701134fcfc9a
khvalbotaihor <ihorkhvalbota@gmail.com>
2021-04-10 15:19:13 +0300

    - moved reducer action to separate variable

--- src/state/user-reducer.test.tsx
+++ src/state/user-reducer.test.tsx
@@ -3,8 +3,8 @@ import {userReducer} from './user-reducer';
 
 test('user reducer should increment only age', () => {
     const startState = { age: 20, childrenCount: 2, name: 'Dimych' };
-
-    const endState = userReducer(startState, { type: 'INCREMENT-AGE' })
+    const action = { type: 'INCREMENT-AGE' }
+    const endState = userReducer(startState, action)
 
     expect(endState.age).toBe(21);
     expect(endState.childrenCount).toBe(2);
@@ -12,7 +12,8 @@ test('user reducer should increment only age', () => {
 
 test('user reducer should increment only childrenCount', () => {
     const startState = { age: 20, childrenCount: 2, name: 'Dimych' };
-    const endState = userReducer(startState, { type: 'INCREMENT-CHILDREN-COUNT' })
+    const action = { type: 'INCREMENT-CHILDREN-COUNT' }
+    const endState = userReducer(startState, action)
 
     expect(endState.age).toBe(20);
     expect(endState.childrenCount).toBe(3);
@@ -21,7 +22,8 @@ test('user reducer should increment only childrenCount', () => {
 test('user reducer should change name of user', () => {
     const startState = { name: 'Dimych', age: 20, childrenCount: 2 };
     const newName = 'Viktor';
-    const endState = userReducer(startState, { type: 'CHANGE-NAME', newName: newName })
+    const action = { type: 'CHANGE-NAME', newName: newName }
+    const endState = userReducer(startState, action)
 
     expect(endState.name).toBe(newName);
 });
